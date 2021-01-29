0f8656f8cdada4f3cc918d754e456055bb229dca
Robert de Bock <robert@meinit.nl>
2021-01-29 10:08:13 +0100

    Remove non-required files, add providers.

--- modules/providers.tf
+++
@@ -1,8 +0,0 @@
-variable "cf_api_key" {}
-
-variable "cf_email" {}
-
-provider "cloudflare" {
-  api_key = var.cf_api_key
-  email   = var.cf_email
-}
--- modules/versions.tf
+++ modules/versions.tf
@@ -1,8 +1,13 @@
 terraform {
+  required_version = ">= 0.13"
   required_providers {
     cloudflare = {
       source = "cloudflare/cloudflare"
+      version = "2.13.2"
+    }
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "2.2.0"
     }
   }
-  required_version = ">= 0.13"
 }
